413f8ebd465b9ac1d4384c28d3b4b19c1c66c271
fabiolatc <61229208+fabiolatc@users.noreply.github.com>
2021-08-16 14:02:11 +0200

    More styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms"
+import './App.css';
 
 export default function Meaning(props) {
     console.log(props)
@@ -9,8 +10,8 @@ export default function Meaning(props) {
         <div className="meaning">
 
 <small><i>{props.meaning.partOfSpeech}</i></small>
-<p><strong>Definition:</strong> {props.meaning.definitions[0].definition}</p>
-<p><strong>Example:</strong> {props.meaning.definitions[0].example}</p>
+<p ><strong>Definition:</strong><span className="definition"> {props.meaning.definitions[0].definition}</span></p>
+<p classNam="example"><strong>Example:</strong> {props.meaning.definitions[0].example}</p>
 
 <Synonyms synonyms={props.meaning.definitions[0].synonyms} />
         </div>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './App.css';
 
 
 export default function Synomyms(props) {
 if (props.synonyms) { return(
 <div className="synonyms">
 
-    {props.synonyms.map(function(synonym, index){return <li key={index}>{synonym}</li>})}
+   <strong>Synonyms:</strong> {props.synonyms.map(function(synonym, index){return <ul className="synonymlist"><li  key={index}>{synonym}</li></ul>})}
 
 </div>
 )
